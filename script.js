7b9183d3fb617bc50a88743e1b5528635144afe5
LeonardoCapocci <leocap2001@gmail.com>
2024-02-06 11:01:04 -0800

    game ends when someone wins (no more placements possible)

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 const gameboardModule = (() => {
   // Creating empty array for gameboard
   const gameboard = ["", "", "", "", "", "", "", "", ""];
-  let currentPlayer
+  let currentPlayer = 'X';
+  let gameActive = true;
 
   // Places mark (X/O) at a certain index spot
-  function placeMark(index, markType) {
-    if (gameboard[index] === "") {
-      gameboard[index] += markType;
-      console.log(gameboard);
-      checkWin();
-
+  function placeMark(index) {
+    if (gameActive) {
+      if (gameboard[index] === "") {
+        gameboard[index] += currentPlayer;
+        switchPlayer()
+        console.log(gameboard);
+        checkWin();
       }
-    else console.log("Choose an empty cell 😡")
-    renderModule.renderGameboard(gameboard)
-    };
+      else console.log("Choose an empty cell 😡");
+      renderModule.renderGameboard(gameboard);
+    }
+    else console.log("You already lost 👎")
+  }
   
   // Switches current player
   function switchPlayer() {
-    if (currentPlayer === "X") currentPlayer = "O"
-    else currentPlayer = "X"
+    if (currentPlayer === 'X') currentPlayer = 'O';
+    else currentPlayer = 'X';
   }
 
   // Checking if the game is over on placement of mark
@@ -34,10 +38,12 @@ const gameboardModule = (() => {
       // X wins
       if (gameboard[a] === 'X' && gameboard[b] === 'X' && gameboard[c] === 'X') {
         console.log("X WINS!");
+        gameActive = false;
       }
       // O Wins
       if (gameboard[a] === 'O' && gameboard[b] === 'O' && gameboard[c] === 'O') {
         console.log("O WINS");
+        gameActive = false;
       }
     }
     checkTie();
@@ -54,12 +60,13 @@ const gameboardModule = (() => {
     }
     if (isTie) {
       console.log("TIE");
+      gameActive = false;
     }
   }
 
   return {
     placeMark: placeMark,
-    gameboard: gameboard,
+    gameboard: gameboard
   };
 })()
 
@@ -73,13 +80,21 @@ const renderModule = (() => {
       gameCell.className = "game-cell";
       gameCell.textContent = cell;
       gameDisplay.appendChild(gameCell);
-    })
+    });
   }
   return {
-    renderGameboard: renderGameboard,
+    renderGameboard: renderGameboard
   };
 })()
 
-gameboardModule.placeMark(0,'X')
-gameboardModule.placeMark(2,'X')
-gameboardModule.placeMark(1,'X')
+renderModule.renderGameboard()
+
+gameboardModule.placeMark(0)
+gameboardModule.placeMark(1)
+gameboardModule.placeMark(8)
+gameboardModule.placeMark(4)
+gameboardModule.placeMark(7)
+gameboardModule.placeMark(2)
+gameboardModule.placeMark(6)
+gameboardModule.placeMark(5)
+gameboardModule.placeMark(3)
